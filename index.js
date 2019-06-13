4a1b2bcd11f0dc5c72d429a6dc7dda4d22dc9bf0
zjmccauley <zjmccauley73@yahoo.com>
2019-06-13 17:09:41 -0600

    index.js change

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-#!/usr/bin/env nodejs
 var http = require('http');
+var fs = require('fs');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  fs.readFile('index.html', (error, html) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(html)
+    res.end();
+  })
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
