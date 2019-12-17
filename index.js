8f2a4d4b39603be3f85450d2bdfd0714ba2c965f
Zachary <Zjmccauley73@yahoo.com>
2019-12-17 13:43:14 -0600

    change

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   fs.readFile('index.html', (error, html) => {
-    Response.writeHead(200, {'Content-Type': 'text/html'});
-    Response.write(html)
-    Response.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html, 'utf-8');
   })
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
