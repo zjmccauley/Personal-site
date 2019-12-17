ae1080e9561b58634973f3f0d6da0946b7d07f22
Zachary <Zjmccauley73@yahoo.com>
2019-12-17 13:13:19 -0600

    change

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-  fs.readFile('index.html', (error, html) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+  fs.readFile('./index.html', (error, html) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(html)
     res.end();
   })
