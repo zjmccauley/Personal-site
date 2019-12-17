d3aa90383c9760cab2989f7afc50bff6474e3d85
Zachary <Zjmccauley73@yahoo.com>
2019-12-17 12:53:40 -0600

    change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   fs.readFile('index.html', (error, html) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
     res.write(html)
     res.end();
   })
